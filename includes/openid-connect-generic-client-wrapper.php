675135c105aeeb383d246955389f2e847ee96659
Greg Brownell <gbrownell@vendasta.com>
2020-05-01 10:11:38 -0600

    Removed + signs from username.

--- includes/openid-connect-generic-client-wrapper.php
+++ includes/openid-connect-generic-client-wrapper.php
@@ -539,7 +539,7 @@ class OpenID_Connect_Generic_Client_Wrapper {
 		}
 
 		// normalize the data a bit
-		$desired_username = strtolower( preg_replace( '/[^a-zA-Z\_0-9]/', '', iconv( 'UTF-8', 'ASCII//TRANSLIT',  $desired_username ) ) );
+		$desired_username = strtolower(str_replace('+', '', $desired_username);
 
 		// copy the username for incrementing
 		$username = $desired_username;

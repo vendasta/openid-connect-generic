79f46b780ece84f5074fb6c0aad3b7b91aceffae
Greg Brownell <gbrownell@vendasta.com>
2020-08-04 15:44:03 -0600

    Fix to login form, so the ajax redirect calls arn't saved in the redirct cookie. This was causing it to me called mutliple times resulting in the error.

--- includes/openid-connect-generic-login-form.php
+++ includes/openid-connect-generic-login-form.php
@@ -76,7 +76,7 @@ class OpenID_Connect_Generic_Login_Form {
 			if ( $GLOBALS['pagenow'] == 'wp-login.php' ) {
 				// if using the login form, default redirect to the admin dashboard
 				$redirect_url = admin_url();
-				if ( isset( $_REQUEST['redirect_to'] ) ) {
+				if ( isset( $_REQUEST['redirect_to'] ) && ( strpos($_REQUEST['redirect_to'], 'admin-ajax') === false )) {
 					$redirect_url = esc_url( $_REQUEST[ 'redirect_to' ] );
 				}
 			}
